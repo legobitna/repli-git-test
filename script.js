627c61215f32d045ed2fd5c9446d197f133d8831
bitna kim <tallshe108@hanmail.net>
2022-02-08 12:43:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let names = [
+  "Steven Paul Jobs",
+  "Bill Gates",
+  "Mark Elliot Zuckerberg",
+  "Elon Musk",
+  "Jeff Bezos",
+  "Warren Edward Buffett",
+  "Larry Page",
+  "Larry Ellison",
+  "Tim Cook",
+  "Lloyd Blankfein",
+];
+
+let firstNames = names.map((item)=>item.split(' ',1))
+console.log(firstNames)
